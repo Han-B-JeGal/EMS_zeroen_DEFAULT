6bc2d2d2c7f381a3ebc68bc96d4ab9836682ebd9
jkhb7 <jkhb7@LAPTOP-1RFSBUTV>
2021-02-05 14:23:00 +0900

    i've written code about connecting Controller to DAO, QUERY

--- src/main/java/apps/dashboard/service/DashboardService.java
+++ src/main/java/apps/dashboard/service/DashboardService.java
@@ -1,9 +1,12 @@
 package apps.dashboard.service;
 
+import java.util.List;
 import java.util.Map;
 
 public interface DashboardService {
-
+	
+	List<Object> dashboardDisplayData() throws Exception;
+	
 	String selectSysdate(Map<String, Object> paraMap);
 
 }
--- src/main/java/apps/dashboard/web/DashboardController.java
+++ src/main/java/apps/dashboard/web/DashboardController.java
@@ -1,16 +1,12 @@
 package apps.dashboard.web;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import apps.dashboard.service.DashboardService;
 
@@ -22,11 +18,17 @@ public class DashboardController {
 	@Autowired
 	DashboardService dashboardService;
 	
-	@RequestMapping(value = "/dashboard002.do")
-	public String dashboard002(HttpServletRequest request, ModelMap model, @RequestParam Map<String, Object> paraMap) throws Exception {
-		logger.debug("dashboard002>>paraMap::" + paraMap.toString());
+//	@RequestMapping(value = "/dashboard002.do")
+//	public String dashboard002(HttpServletRequest request, ModelMap model, @RequestParam Map<String, Object> paraMap) throws Exception {
+//		logger.debug("dashboard002>>paraMap::" + paraMap.toString());
+//		
+//		return "apps/dashboard002.jsp";
+//	}
+
+	@RequestMapping(value="/ems_zeroen")
+	public String displayData(List<Object> resultData) throws Exception{
+		List<Object> dashboardData = dashboardService.dashboardDisplayData();
 		
 		return "apps/dashboard002.jsp";
 	}
-
 }
